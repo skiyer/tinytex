c6c42dc65555eb2303f7ffbae44985a4cb7336d3
skiyer <skiyer@users.noreply.github.com>
2018-04-19 12:11:08 +0800

    fix url

--- tools/install-unx.sh
+++ tools/install-unx.sh
@@ -10,14 +10,14 @@ else
   alias download='wget -qO-'
 fi
 
-download https://github.com/yihui/tinytex/raw/master/tools/install-base.sh | sh -s - "$@"
+download https://github.com/skiyer/tinytex/raw/master/tools/install-base.sh | sh -s - "$@"
 
 rm -rf $TEXDIR
 mkdir -p $TEXDIR
 mv texlive/* $TEXDIR
 rm -r texlive
 
-$TEXDIR/bin/*/tlmgr install $(download https://github.com/yihui/tinytex/raw/master/tools/pkgs-custom.txt | tr '\n' ' ')
+$TEXDIR/bin/*/tlmgr install $(download https://github.com/skiyer/tinytex/raw/master/tools/pkgs-skiyer.txt | tr '\n' ' ')
 
 if [ "$1" = '--admin' ]; then
   if [ "$2" != '--no-path' ]; then
